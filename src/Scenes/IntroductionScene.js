a1d633c1dceb536b37287251dec059f4b3682a0e
Expedito Andrade <expeditojazz@gmail.com>
2020-07-20 05:00:24 -0300

    finish introPage

--- src/Scenes/IntroductionScene.js
+++ src/Scenes/IntroductionScene.js
@@ -8,9 +8,45 @@ export default class IntroductionScene extends Phaser.Scene {
 
   create() {
     this.cameras.main.setBackgroundColor('#000111');
-    this.introText = this.add.tileSprite(0, 0, 'Introduction', { fontSize: '30px', fill: '#fff' });
+    this.introText = this.add.text(0, 0, 'Introduction', { fontSize: '32px', fill: '#fff' });
     this.madeByText = this.add.text(0, 0, 'We live in a strange world where some people mistreat they little pets. John, our hero works in a non-profit organization where he needs to save any kind of animals that are hurt. It does not matter where (sometimes a city, a florest, or even a desert), our hero is always there to do whatever it takes. Sometimes the pets are too heart and need medical attention really quick. Can he fill the task and save the little guys?');
-    this.keySpace = this.input.keyboard.addKey(Phase.Input.Keyboard.KeyCodes.SPACE);
-    this.skipText = this.add.text(10, 10, 'Press SPACE\nto skip', { fontSize: '12px', fill: '#fff' });
+    this.zone = this.add.zone(800 / 2, 600 / 2, 800, 600);
+
+    this.keySpace = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+    this.skipText = this.add.text(10, 10, 'Press SPACE\nto skip', { fontSize: '10px', fill: '#fff' });
+
+    Phaser.Display.Align.In.Center(
+      this.introText,
+      this.zone,
+    );
+
+    Phaser.Display.Align.In.Center(
+      this.madeByText,
+      this.zone,
+    );
+    this.madeByText.setY(650);
+
+    this.introTween = this.tweens.add({
+      targets: this.introText,
+      y: -200,
+      duration: 3000,
+      delay: 1000,
+    });
+
+    this.madeByTween = this.tweens.add({
+      targets: this.madeByText,
+      y: -500,
+      duration: 55000,
+      delay: 0,
+      onComplete: (() => {
+        this.scene.start(('Game'));
+      }),
+    });
+  }
+
+  update() {
+    if (this.keySpace.isDown) {
+      this.scene.start('Game');
+    }
   }
 }

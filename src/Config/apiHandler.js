a719ede3cd6ae3a02d2f7c2d8cd245b2c8cab7ae
Expedito Andrade <expeditojazz@gmail.com>
2020-07-21 12:15:46 -0300

    remove comments from API

--- src/Config/apiHandler.js
+++ src/Config/apiHandler.js
@@ -10,7 +10,7 @@ const apiHandlerPostFirst = async () => {
 
 const getScore = async () => {
   const res = await axios.get('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/wCfaFqsAfRaTh8oi5OR1/scores/');
-  console.log(res);
+  return res.data;
 };
 
 const postScore = async (user, score) => {
@@ -23,31 +23,10 @@ const postScore = async (user, score) => {
   const res = await axios.post('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/wCfaFqsAfRaTh8oi5OR1/scores/', body, {
     headers: options,
   });
-  console.log(res);
+  return res.data;
 };
-
-// const apiExport = async (name, score) => {
-//   const key = await apiHandlerPostFirst();
-//   const firstPost = await postScore(name, score, key.data.result.split(':')[1].split(' ')[0]);
-//   const firstScore = await getScore(key.data.result.split(' ')[1]);
-//   console.log('belo is the first get');
-//   console.log(firstScore);
-//   console.log('below is the post request');
-//   console.log(firstPost);
 
 
 export default {
   apiHandlerPostFirst, getScore, postScore,
 };
-
-
-// axios.post('/user', {
-//   firstName: 'Fred',
-//   lastName: 'Flintstone'
-// })
-// .then(function (response) {
-//   console.log(response);
-// })
-// .catch(function (error) {
-//   console.log(error);
-// });

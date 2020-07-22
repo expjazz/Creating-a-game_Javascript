d04cc5d5cb48a4fecb16d14573e3302de1d33899
Expedito Andrade <expeditojazz@gmail.com>
2020-07-22 18:41:40 -0300

    add jest es6 imports

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,19 +1,18 @@
-import config from './Config/config';
-import GameScene from './Scenes/GameScene';
-import BootScene from './Scenes/BootScene';
-import PreloaderScene from './Scenes/PreloaderScene';
-import TitleScene from './Scenes/TitleScene';
-import OptionsScene from './Scenes/OptionsScene';
-import CreditsScene from './Scenes/CreditsScene';
-import IntroductionScene from './Scenes/IntroductionScene';
-import MidDialogue from './Scenes/MidDialogue';
-import GameOver from './Scenes/GameOver';
-import back from './Scenes/background';
-import saveUserRecord from './Scenes/saveUserRecord';
-import HighScoreScene from './Scenes/HighScoreScene';
-import lines from './Scenes/dialogues';
-import FreePlay from './Scenes/FreePlay';
-import Audio from './models/music';
+import GameScene from '../src/Scenes/GameScene';
+// import BootScene from './Scenes/BootScene';
+// import PreloaderScene from './Scenes/PreloaderScene';
+// import TitleScene from './Scenes/TitleScene';
+// import OptionsScene from './Scenes/OptionsScene';
+// import CreditsScene from './Scenes/CreditsScene';
+// import IntroductionScene from './Scenes/IntroductionScene';
+// import MidDialogue from './Scenes/MidDialogue';
+// import GameOver from './Scenes/GameOver';
+import back from '../src/Scenes/background';
+// import saveUserRecord from './Scenes/saveUserRecord';
+// import HighScoreScene from './Scenes/HighScoreScene';
+// import lines from './Scenes/dialogues';
+// import FreePlay from './Scenes/FreePlay';
+// import Audio from './models/music';
 
 it('should be able to create a new scene', () => {
   const newScene = new GameScene('One', back.background[0], 'rat', 'DialogueOne', 350, 30000, 'one', 'platform', 3.2);

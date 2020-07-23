61ff6442336874052e2d1845c46395ec4bb5de6b
Expedito Andrade <expeditojazz@gmail.com>
2020-07-23 08:14:46 -0300

    add scoreAPI tests

--- __test__/scoreboardApi.test.js
+++ __test__/scoreboardApi.test.js
@@ -4,3 +4,7 @@ import api from '../src/Config/apiHandler';
 it('should be able to save the score in the microverse backend', () => {
   api.postScore('test', 10).then((score) => expect(score).toBe('Leaderboard score created correctly.'));
 });
+
+it('should be able to retrieve the store as an object', () => {
+  api.getScore().then((scores) => expect(typeof scores).toBe('object'));
+});
